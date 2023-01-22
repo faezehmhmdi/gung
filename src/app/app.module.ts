41120f3e8521aba0ebdf10a07233c760cb55a4ec
Faezeh <faezeh.mohammadi78@gmail.com>
2023-01-22 19:31:01 +0100

    category component created, service files added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule
